c90321ebd703188756731f89a4857cd6226e0d3c
rarun700 <rarun700@gmail.com>
2022-03-23 10:11:37 +0530

    initialCommit

---
+++ src/Layout/AuthLayout/AuthLayout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./AuthLayout.module.css";
+
+function AuthLayout() {
+  return <div>AuthLayout</div>;
+}
+
+export default AuthLayout;
---
+++ src/Layout/HomeLayout/HomeLayout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "HomeLayout.module.css";
+
+function HomeLayout() {
+  return <div>HomeLayout</div>;
+}
+
+export default HomeLayout;
